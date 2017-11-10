5b924cd9036a58eb25784d4fc3c382f86c693751
mcgml <mcgml@cardiff.ac.uk>
2017-11-10 09:48:18 +0000

    version bump

--- 1_SomaticAmplicon.sh
+++ 1_SomaticAmplicon.sh
@@ -8,7 +8,7 @@ cd $PBS_O_WORKDIR
 #Description: Somatic Amplicon Pipeline (Illumina paired-end). Not for use with other library preps/ experimental conditions.
 #Author: Matt Lyon, All Wales Medical Genetics Lab
 #Mode: BY_SAMPLE
-version="1.7.1"
+version="1.7.2"
 
 # Directory structure required for pipeline
 #
